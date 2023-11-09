6214a3fe14ad32ec8e178ab913bf690a866969da
Cassandra Tin <cassandra.tinkwonyuen@mail.utoronto.ca>
2023-11-09 16:36:03 -0500

    Added requirements

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,10 +1,5 @@
 python3 -m venv venv
 source venv/bin/activate
-pip install pip==23.3.1
-pip install django==4.2
-pip install pillow==10.0
-pip install djangorestframework
-pip install django-filter
-pip install djangorestframework-simplejwt
+pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
