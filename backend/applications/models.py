b2af3db39a19fe701fc4d4ddef6e658fa25e7eb1
MalHus <malaikahhus@gmail.com>
2023-11-12 14:45:25 -0500

    small changes application models and renaming

--- backend/applications/models.py
+++ backend/applications/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from accounts.models.SeekerModel import Seeker
 from accounts.models.ParentUserModel import ParentUser
+from pets.models.PetDetailModel import Pet
+from django.utils import timezone
 
 # Create your models here.
 class Application(models.Model):
@@ -49,7 +51,7 @@ class Application(models.Model):
 
     # should not be deleted unless the pet itself is gone.
     adopter_id = models.ForeignKey(Seeker, on_delete=models.SET_DEFAULT, default='deleted_user') # seeker never can access their page so this won't be a problem, shelter will just see deleted_user
-    pet_id = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    pet_id = models.ForeignKey(Pet, on_delete=models.CASCADE, null=True)
     status = models.CharField(max_length=1, choices=status_choices)
     email = models.EmailField()
     phone = models.PositiveBigIntegerField()
@@ -68,7 +70,7 @@ class Application(models.Model):
     vet_contact = models.PositiveBigIntegerField(null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
     last_update = models.DateTimeField()
-    creation_time = models.DateTimeField()
+    creation_time = models.DateTimeField(default=timezone.now)
 
     def save(self, *args, **kwargs):
         # Should only be true if the Application is first being made, 
