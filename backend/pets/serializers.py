49bf452f1e744307cb93747ccee95eac87dcf5de
MalHus <malaikahhus@gmail.com>
2023-11-13 12:33:51 -0500

    added validate function to pet-details

--- backend/pets/serializers.py
+++ backend/pets/serializers.py
@@ -1,8 +1,16 @@
 
 from rest_framework import serializers
 from .models.PetDetailModel import PetDetail
+from rest_framework.response import Response
 
 class PetSerializer(serializers.ModelSerializer):
   class Meta:
     model = PetDetail
-    fields = '__all__'
+    fields = ['name', 'status', 'gender', 'age', 'colour', 'breed', 'location', 'size', 'behaviour', 'description', 'medical_history', 'pet_image1', 'pet_image2', 'pet_image3', 'pet_image4']
+
+  def validate(self, data):
+    required_fields = ['name', 'status', 'gender', 'age', 'colour', 'breed', 'location', 'size', 'behaviour', 'pet_image1', 'pet_image2', 'pet_image3', 'pet_image4']
+
+    for field in required_fields:
+      if field not in data or not data[field]:
+        return Response({"detail": f"The {field} field is required"})
