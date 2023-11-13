9718e51bf5f16883e9177b8312b0e9dfc13c279b
MalHus <malaikahhus@gmail.com>
2023-11-13 12:36:25 -0500

    added create view to pet details

--- backend/pets/urls.py
+++ backend/pets/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 app_name='pet'
+
 urlpatterns = [
   path('create/', PetViewSet.as_view({
     'post':'create'
--- backend/pets/views.py
+++ backend/pets/views.py
@@ -1,9 +1,34 @@
 from rest_framework import viewsets
 from .models.PetDetailModel import PetDetail
 from .serializers import PetSerializer
-from rest_framework.views import APIView
+from rest_framework.response import Response
+from accounts.models.ShelterModel import Shelter
 
 
 class PetViewSet(viewsets.ModelViewSet):
   queryset = PetDetail.objects.all()
   serializer_class = PetSerializer
+
+  def perform_create(self, serializer):
+
+    # Note: 4 images of the animal are required and all fields (except medical history and description) are required
+
+    # Only shelters can submit applications
+    if not isinstance(self.request.user, Shelter):
+      return Response({"detail": "Seekers cannot submit applications"}, status=403)
+
+    # A shelter must be signed in to create a pet-listing
+    if not self.request.user.is_autheticated:
+      return Response({"detail": "Authetication failed"}, status=403)
+
+    # A shelter cannot create/list the same pet
+    name = serializer.validated_data['name']
+    breed = serializer.validated_data['breed']
+    shelter = serializer.validated_data['shelter']
+
+    if PetDetail.objects.filter(name=name, breed=breed, shelter=shelter).exists():
+      return Response({"detail": "This shelter already has a pet with this name and breed. Duplicate pet postings not allowed."}, status=403)
+    
+    # if all conditions are met, save 
+    serializer.save()
+    
