a948ad88371326a2b76c621615fb7180387dac95
daexs <daelenia.susanto@mail.utoronto.ca>
2023-11-12 22:58:38 -0500

    Made views and serializers for applications. Also modified pet model.

---
+++ backend/applications/serializers.py
@@ -0,0 +1,67 @@
+from rest_framework.serializers import ModelSerializer, CharField, PrimaryKeyRelatedField, ChoiceField
+from applications.models import Application 
+from django.shortcuts import get_object_or_404
+
+# All the validation is done in the view, all you do here is to handle how to display
+class ApplicationSerializer(ModelSerializer):
+
+    adopter_id = PrimaryKeyRelatedField(read_only = True)
+    pet_id = PrimaryKeyRelatedField(read_only = True)
+
+    status_choices = [
+            ('P', 'Pending'),
+            ('Y', 'Accepted'),
+            ('D', 'Declined'),
+            ('W', 'Withdrawn'),
+    ]
+
+    province_choices = [
+            ('ON', 'Ontario'),
+            ('SK', 'Saskatchewan'),
+            ('AL', 'Alberta'),
+            ('NB', 'New Brunswick'),
+            ('BC', 'British Columbia'),
+            ('NW', 'Newfoundland and Labrador'),
+            ('MB', 'Manitoba'),
+            ('NS', 'Nova Scotia'),
+            ('PE', 'Prince Edward Island'),
+            ('QB', 'Quebec'),
+            ('NU', 'Nunavut'),
+            ('YK', 'Yukon'),
+            ('NT', 'Northwest Territories'),
+    ]
+
+    home_types = [
+        ('SFH', 'Single-Family Home'),
+        ('APT', 'Apartment'),
+        ('CON', 'Condominium'),
+        ('TNH', 'Townhouse'),
+        ('DUP', 'Duplex'),
+        ('TRI', 'Triplex'),
+        ('MOH', 'Mobile Home'),
+        ('TIN', 'Tiny House'), 
+        ('MAN', 'Mansion'),
+        ('BUN', 'Bungalow'),
+        ('LOG', 'Log House'),
+        ('FLO', 'Floating Home'),
+        ('IGL', 'Igloo'),
+    ]   
+
+    pet_name = CharField(max_length=20, read_only=True)
+
+    # Shows user human-readable portion of the tuples
+    status = ChoiceField(choices=status_choices)
+    province = ChoiceField(choices=province_choices)
+    home = ChoiceField(choices=home_types)
+
+    class Meta:
+        model = Application
+        exclude = ['last_update', 'creation_time']
+
+
+    def __init__(self, *args, **kwargs):
+        pet_id = self.context.get('pet_id')
+        pet = get_object_or_404(Pet, id=pet_id)
+        super(ApplicationSerializer, self).__init__(*args, **kwargs)
+
+        self.fields['pet_name'].default = pet.name
--- backend/applications/urls.py
+++ backend/applications/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from backend.applications.views import ApplicationListCreateView
 
 # Make sure endpoints only have nouns to indicate it is stateless (RESTful convention)
 app_name = 'application'
 urlpatterns = [
     # to redirect/reverse, format is 'namespace:name'
+    path('pet/<int:pet_id>/', ApplicationListCreateView.as_view(), name='create_application'),
     # path('pet/<int:pk>/user/<int:id>/', views, name='details'),
     # path('user/<int:pk>/list/', views, name='list'),
     # path('user/<int:pk>/list/filtration/', views, name='filtration'),
