0d073021b098a95e16536036600b416061de1f80
daexs <daelenia.susanto@mail.utoronto.ca>
2023-11-13 21:02:40 -0500

    modified to get user_type from backend

--- backend/applications/urls.py
+++ backend/applications/urls.py
@@ -6,7 +6,7 @@ app_name = 'application'
 urlpatterns = [
     # to redirect/reverse, format is 'namespace:name'
     path('pet/<int:pet_id>/', ApplicationCreateView.as_view(), name='create_application'),
-    path('<str:user_type>/list/', ApplicationListView.as_view(), name='list_applications'),
-    path('<str:user_type>/list/status/<str:status>/time/<str:type>/', ApplicationListFilterView.as_view(), name='list_filter'),
-    path('<str:user_type>/<int:app_id>/', ApplicationRetrieveUpdateView.as_view(), name='view_application'),
+    path('list/', ApplicationListView.as_view(), name='list_applications'),
+    path('list/status/<str:status>/time/<str:type>/', ApplicationListFilterView.as_view(), name='list_filter'),
+    path('<int:app_id>/', ApplicationRetrieveUpdateView.as_view(), name='view_application'),
 ]
