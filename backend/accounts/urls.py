0a60eac7d26bbc02561f29135158646b9b77ee73
Elena Scobici <elena@scobici.com>
2023-11-14 15:17:10 -0500

    Allow user to provide profile picture

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'accounts'
 urlpatterns = [
-    # to redirect/reverse, format is 'namespace:name'
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/seeker/', views.RegisterSeekerView.as_view(), name='register_seeker'),
     path('register/shelter/', views.RegisterShelterView.as_view(), name='register_shelter'),
     path('update/', views.UpdateAccountView.as_view(), name='update_account'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
