2b31c61edbc364dc156b160e65cf6fc27973e189
Cassandra Tin <cassandra.tinkwonyuen@mail.utoronto.ca>
2023-11-15 13:02:53 -0500

    Changed urls

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name="comments"
 urlpatterns = [
-    path('shelter/<int:shelter_id>/details/<int:user_id>/comments/', views.CommentListCreate.as_view(), name='comment'),
-    path('shelter/<int:shelter_id>/details/<int:user_id>/review/', views.ReviewCreate.as_view(), name='review'),
-    path('application/<int:pk>/<int:user_id>/messages', views.MessageListCreate.as_view(), name='messages')
+    path('shelter/<int:shelter_id>/details/comments/', views.CommentListCreate.as_view(), name='comment'),
+    path('shelter/<int:shelter_id>/details/review/', views.ReviewCreate.as_view(), name='review'),
+    path('application/<int:pk>/messages', views.MessageListCreate.as_view(), name='messages')
 ]
