1aadbfe68467057da1691d44582ce92f8b645b14
Cassandra Tin <cassandra.tinkwonyuen@mail.utoronto.ca>
2023-12-06 12:33:15 -0500

    Created header and footer layout

--- P3/frontend/src/App.js
+++ P3/frontend/src/App.js
@@ -2,15 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Layout from './components/Layout'
 
 function App() {
   return (
    <BrowserRouter>
     <Routes>
-      {/* <Route path="/" element={<Layout />}> */}
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
-      {/* </Route> */}
+        {/* <Route path="*" element={<NotFound />} />  */}
+      </Route>
     </Routes>
   </BrowserRouter>
   );
