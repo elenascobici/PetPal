41fc13b2d249a0e431167cab929659e45375086f
Cassandra Tin <cassandra.tinkwonyuen@mail.utoronto.ca>
2023-12-06 21:23:58 -0500

    Fixed navbar menu dropdown

--- P3/frontend/src/App.js
+++ P3/frontend/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Layout from './components/Layout'
