90e0ef9daa97026a34bd858595eadea1fe4175af
daexs <daelenia.susanto@mail.utoronto.ca>
2023-12-06 19:57:16 -0500

    added url for application form

--- P3/frontend/src/App.js
+++ P3/frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Layout from './components/Layout'
 import NotFound from './pages/NotFound';
+import ApplicationForm from './pages/ApplicationForm';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="*" element={<NotFound />} /> 
+        <Route path="application/form" element={<ApplicationForm />} />
       </Route>
     </Routes>
   </BrowserRouter>
