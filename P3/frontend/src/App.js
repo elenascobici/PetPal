f0e5ab3d8065500ae647fb85fab2826faa322585
Elena Scobici <elena@scobici.com>
2023-12-06 21:07:07 -0500

    Add signup to App.js

--- P3/frontend/src/App.js
+++ P3/frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import SignUp from './pages/SignUp/SignUp';
 import Layout from './components/Layout'
 import NotFound from './pages/NotFound';
 import ApplicationForm from './pages/ApplicationForm';
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="sign-up/" element={<SignUp />} />
         <Route path="*" element={<NotFound />} /> 
         <Route path="application/form" element={<ApplicationForm />} />
       </Route>
