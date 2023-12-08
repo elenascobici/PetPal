e3d3db859005fa0fcf1929898541c9720a95e2b2
Elena Scobici <elena@scobici.com>
2023-12-07 21:50:49 -0500

    Add view profile page and url

--- P3/frontend/src/App.js
+++ P3/frontend/src/App.js
@@ -12,6 +12,7 @@ import LogOut from './pages/LogOut/LogOut';
 import Layout from './components/Layout'
 import NotFound from './pages/NotFound';
 import ApplicationForm from './pages/ApplicationForm';
+import { ViewProfile } from './pages/Profile/ViewProfile'; 
 
 function App() {
   return (
@@ -24,9 +25,9 @@ function App() {
         <Route path="sign-up-shelter/" element={<SignUpShelter />} />
         <Route path="log-in/" element={<Login />} />
         <Route path="log-out/" element={<LogOut />} />
+        <Route path="profile/" element={<ViewProfile />} />
         <Route path="*" element={<NotFound />} /> 
         <Route path="application/form" element={<ApplicationForm />} />
-        <Route path="*" element={<NotFound />} /> 
       </Route>
     </Routes>
   </BrowserRouter>
