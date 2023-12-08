d154c741127bfd2ef8fad71496cccc66a658dcdd
Elena Scobici <elena@scobici.com>
2023-12-07 21:23:49 -0500

    Change navbar when signed in

--- P3/frontend/src/components/Layout/NavBar/NavButtons/index.jsx
+++ P3/frontend/src/components/Layout/NavBar/NavButtons/index.jsx
@@ -12,7 +12,7 @@ const NavButtons = ({ userType }) => {
 
     return (
         <ul className="nav navbar-nav navbar-right px-2" id="navButtons">
-            {userType ? (
+            {isAuth ? (
                 <>
                 <li><Link to="/profile" className="yellowButton me-3" id="signUp">Profile</Link></li>
                 <li><div id="empty"> </div></li>
