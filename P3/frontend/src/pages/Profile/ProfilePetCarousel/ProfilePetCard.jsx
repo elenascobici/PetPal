0f9ed54be4a1f89769272b09c103ebac203b142d
Elena Scobici <elena@scobici.com>
2023-12-08 21:18:05 -0500

    Fetch pet images from server

--- P3/frontend/src/pages/Profile/ProfilePetCarousel/ProfilePetCard.jsx
+++ P3/frontend/src/pages/Profile/ProfilePetCarousel/ProfilePetCard.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import DefaultPet from '../../../assets/images/default-pet.png'
 
 const ProfilePetCard = ({ link, name, image }) => {
+    // Fetch pet picture.
+    const [picture, setPicture] = React.useState(DefaultPet);
+    React.useEffect(() => {
+        if (image) {
+            setPicture("http://localhost:8000/pet/pet-image/" + image.split('/').pop());
+        }
+
+    }, [image, picture])
+
     return (
-        <div className="grid-item" >
+        <div className="pet-grid-item" >
         {name ? (
                 <Link to={link} className="pet">
-                    <img className="petImage" src={image}/>
+                    <img className="petImage" src={picture}/>
                     <div className="petLabel">{name}</div>
                 </Link>
         ) : (
