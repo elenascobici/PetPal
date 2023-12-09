b6d83380d3877efe4970c0de6d2cdb62d49cd91e
daexs <daelenia.susanto@mail.utoronto.ca>
2023-12-09 00:30:06 -0500

    Fixed Logic Application Form

--- P3/frontend/src/pages/ApplicationForm/Form/Alert/index.jsx
+++ P3/frontend/src/pages/ApplicationForm/Form/Alert/index.jsx
@@ -5,19 +5,13 @@ import Alert from 'react-bootstrap/Alert';
 
 import React, { useState } from 'react';
 
-function AlertPopup(){
-    const [close, setShow] = useState(true);
-
-    const closeAlert = (event) => {
-        setShow(false);
-    };
-    
+function AlertPopup({errorMsg}){
 
     return <>
     {/* {console.log("HERE")} */}
         <div className="container mt-5">
          <Alert key='danger' variant='danger'>
-            Please fill out all the required fields.
+            {errorMsg}
         </Alert>
         </div>
     </>;
--- P3/frontend/src/pages/ApplicationForm/index.jsx
+++ P3/frontend/src/pages/ApplicationForm/index.jsx
@@ -2,13 +2,33 @@
 import Form from "./Form";
 import './style.css';
 import catImg from '../../assets/images/bg-adoption.jpg';
-// 1. Create a React Component
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+
 function ApplicationForm(){
-    // 2. Figure out what you want to handle (do this in the form html)
-    // - Validation and changing CSS
-    // - When submit is clicked, send an ajax req.
-    // - navigate to the list page with a message on the top (can be clicked out so maybe an element in list and if shown then onclick x remove and hide?)
-    //! RETRIEVE PET ID SOMEHOW
+    const {petID}= useParams();
+    const [pet_data, setPetData] = useState({});
+
+    useEffect(() => {
+          const token = localStorage.getItem('access_token');
+          fetch(`http://localhost:8000/pet/${petID}/`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log('Pet data:', data);
+                setPetData(data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }, []);
 
     return <>
        <div className="image-wrapper">
@@ -19,7 +39,7 @@ function ApplicationForm(){
             <div className="container mt-5">
                 <h2 className="text-start mb-4 mt-4 title">General Pet Adoption Application</h2>
                 <h4 className="text-start mb-4">Adopter's Details</h4>
-               <Form petID={4}/>
+               <Form petID={petID}/>
             </div>
        </div>
      </>;
