2ee7a1c28a4c3aa688070e2469feeeee1d4ba5d7
Elena Scobici <elena@scobici.com>
2023-12-08 23:46:28 -0500

    Create likes

--- P3/backend/blogs/urls.py
+++ P3/backend/blogs/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name="blogs"
 urlpatterns = [
-    path('new-blog/', views.BlogCreate.as_view(), name='create-blog')
+    path('new-blog/', views.BlogCreate.as_view(), name='create-blog'),
+    path('like/<int:blog_id>/', views.LikeCreate.as_view(), name='create-like'),
 ]
--- P3/backend/blogs/views.py
+++ P3/backend/blogs/views.py
@@ -1,8 +1,28 @@
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
-from .serializers import BlogSerializer
-from .permissions import BlogCreatePermission
+from .serializers import BlogSerializer, LikeSerializer
+from .permissions import BlogCreatePermission, LikePermission
+from .models import Blog
 
 class BlogCreate(CreateAPIView):
     serializer_class = BlogSerializer
     permission_classes = [IsAuthenticated, BlogCreatePermission]
+
+class LikeCreate(CreateAPIView):
+    serializer_class = LikeSerializer
+    permission_classes = [IsAuthenticated, LikePermission]
+
+    def perform_create(self, serializer):
+        # Get the blog_id from the query parameters
+        blog_id = self.kwargs.get('blog_id')
+
+        # Get the corresponding blog instance
+        blog = Blog.objects.get(pk=blog_id)
+
+        # Increase the likes count of the blog by 1
+        blog.likes += 1
+        blog.save()
+
+        # Set the serializer's validated data with the blog instance for Like creation
+        serializer.validated_data['blog'] = blog
+        serializer.save()
