01537c76f71e9ae32be019f218ae8affb06e8c3d
Elena Scobici <elena@scobici.com>
2023-12-11 14:27:58 -0500

    Add vercel to allowed origins

--- P3/backend/petpal/settings.py
+++ P3/backend/petpal/settings.py
@@ -158,6 +158,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  
     "http://127.0.0.1:3000",
+    "https://pet-pal-vercel.vercel.app",
 ]
 
 from datetime import timedelta
