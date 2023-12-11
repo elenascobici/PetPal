f06e43a257d4d65b06774790da39c0d1289c0d51
MalHus <malaikahhus@gmail.com>
2023-12-11 15:19:03 -0500

    images on pet  search for seeker

--- P3/frontend/src/pages/PetSearchSeeker/PetCard/index.jsx
+++ P3/frontend/src/pages/PetSearchSeeker/PetCard/index.jsx
@@ -4,19 +4,18 @@ import { useNavigate } from 'react-router-dom';
 
 function PetCard({ pet }) {
   const navigate = useNavigate();
-  // const imageUrl = `https://petpal-production.up.railway.app/media/pets/${pet.pet_image_1}`;
+  const imageUrl = `https://petpal-production.up.railway.app/pet/pet-image/` + (typeof pet.pet_image_1 === 'string' ? pet.pet_image_1.split('/').pop() : '');
 
-
+  
   return (
     <div className="grid-item grid-item-inner">
       <Link to={`/pets/${pet.id}`} className="pet">
-        {/* <img className="petImage" src={imageUrl} alt={pet.name} /> */}
+        <img className="petImage" src={imageUrl} alt={pet.name} />
         <div className="petLabel">{pet.name}</div>
       </Link>
       <p className="status">
         {pet.status}
       </p>
-      
     </div>
   );
 }
