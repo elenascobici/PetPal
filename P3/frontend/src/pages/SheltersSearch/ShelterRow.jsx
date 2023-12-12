3b46c994f736e6c6298d1fa868ae8d68e66db35b
Elena Scobici <elena@scobici.com>
2023-12-12 13:57:26 -0500

    Fix location in shelter table

--- P3/frontend/src/pages/SheltersSearch/ShelterRow.jsx
+++ P3/frontend/src/pages/SheltersSearch/ShelterRow.jsx
@@ -6,12 +6,26 @@ const ShelterRow = ({shelter}) => {
 
     const shelterLink = "/shelter/" + shelter.id + "/" + shelter.name; 
 
+    function format_location(street, city, province) {
+        const location = [];
+        if (street) {
+            location.push(street);
+        }
+        if (city) {
+            location.push(city);
+        }
+        if (province) {
+            location.push(province);
+        }
+        return location.join(', ');
+    }
+
     return (
         <>
         <div class="grid-item grid-item-shelter shelterLink">
         <a class="shelterLink" href={shelterLink}>{shelter.name}</a>
         </div>
-        <div class="grid-item grid-item-shelter">{(shelter.street? shelter.street: '') + ' ' + (shelter.city ? shelter.city: '' + ' ') + shelter.province}</div>
+        <div class="grid-item grid-item-shelter">{format_location(shelter.street, shelter.city, shelter.province)}</div>
         <div class="grid-item grid-item-shelter">
         <a class="email-shelter" href={mailTo}>{shelter.email}</a> 
         </div>
